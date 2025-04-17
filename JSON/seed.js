cd636fd7f69a17233bb8f0bfaa03b3a71e76bc77
Miickkk <anickmurilomeyretuti@gmail.com>
2025-04-17 11:26:51 -0300

    17.04.25

---
+++ JSON/seed.js
@@ -0,0 +1,20 @@
+const { faker } = require("@faker-js/faker");
+const fs = require("fs");
+
+const generateData = () => {
+  const users = [];
+
+  for (let i = 0; i < 1000; i++) {
+    users.push({
+      id: i + 1,
+      name: faker.name.fullName(),
+      email: faker.internet.email(),
+    });
+  }
+
+  return { users };
+};
+
+const data = generateData();
+
+fs.writeFileSync("db.json", JSON.stringify(data, null, 2));
