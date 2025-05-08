b46ff541d391e20090cfcffc85e6e8cb0b72f490
Miickkk <anickmurilomeyretuti@gmail.com>
2025-05-08 11:34:38 -0300

    08.05.25

--- guiapress/articles/Article.js
+++ guiapress/articles/Article.js
@@ -1,22 +1,29 @@
 const Sequelize = require("sequelize");
-const connection = require ("../database/database");
+const connection = require("../database/database");
 const Category = require("../categories/Category");
 
-const Article = connection.define('articles', {
-    title:{
+
+const Article = connection.define("articles", {
+    title: {
         type: Sequelize.STRING,
         allowNull: false
-    }, slug: {
+    },
+    slug: {
         type: Sequelize.STRING,
         allowNull: false
     },
     body: {
         type: Sequelize.TEXT,
         allowNull: false
+    },
+    categoryId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
     }
-})
+});
 
-Category.hasMany(Article);
-Article.belongsTo(Category);
+
+Category.hasMany(Article);       // 1 categoria → muitos artigos
+Article.belongsTo(Category);  // 1 artigo → 1 categoria
 
 module.exports = Article;
--- guiapress/categories/Category.js
+++ guiapress/categories/Category.js
@@ -1,16 +1,13 @@
 const Sequelize = require("sequelize");
-const connection = require ("../database/database");
+const connection = require("../database/database");
 
 const Category = connection.define('categories', {
     title:{
         type: Sequelize.STRING,
         allowNull: false
-    }, slug: {
-        type: Sequelize.STRING,
-        allowNull: false
     },
-    body: {
-        type: Sequelize.TEXT,
+    slug:{
+        type: Sequelize.STRING,
         allowNull: false
     }
 })
